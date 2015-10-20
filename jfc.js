9436d089206e54babbcaaee3cea5b9ecf72555cb
Yank Daddy <jackesilver@gmail.com>
2015-10-20 18:28:26 -0400

    first working iteration of responsive divs

--- jfc.js
+++ jfc.js
@@ -1,36 +1,50 @@
 window.onload = function() {
+
 var container = document.getElementById("container");
+
 var row1 = document.createElement("div");
 var row2 = document.createElement("div");
 var row3 = document.createElement("div");
 var row4 = document.createElement("div");
+
 row1.setAttribute("class", "row");
 row2.setAttribute("class", "row");
 row3.setAttribute("class", "row");
 row4.setAttribute("class", "row");
+
 container.appendChild(row1);
 container.appendChild(row2);
 container.appendChild(row3);
 container.appendChild(row4);
+
 for (var i = 1; i <= 24; i++) {
+
 	var element = document.createElement("div");
+
 	element.setAttribute("class", "red2");
+
+	// console.log(this);
 		if(i > 18) {
 		row1.appendChild(element);	
 		} else if(i > 12) {
 		row2.appendChild(element);	
 		} else if(i > 6) {
+			// console.log("butt")
 		row3.appendChild(element);	
 		} else if(i > 0) {
-			console.log("hi")
+			// console.log("hi")
 		row4.appendChild(element);
-		}
+		} element.addEventListener("click", gotClicked);
 };
-
+	
 	var redDiv = document.querySelector('.red2');
-	console.log(redDiv);
-	redDiv.addEventListener("click", gotClicked);
+	// element.addEventListener("click", gotClicked);
 	function gotClicked() {
-		var AAA = prompt("got clicked");
+		console.log(this);
+		var seatIsReserved = window.confirm("Click 'OK' to reserve" + " " +  + " " + "");
+				console.log(element);
+				if(seatIsReserved === true) {
+					this.setAttribute("class", "isReserved");
+				}
 	}
- }
+ };
